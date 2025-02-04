7583ae129cde0ee22e6a153d0dfaae2da394c0e1
JASON V. CASTELLANO <JASON@cc.usls.edu>
2025-02-04 11:12:11 +0800

    Done with inventory table

--- resources/views/inventory/index.blade.php
+++ resources/views/inventory/index.blade.php
@@ -18,34 +18,31 @@
         <div class="row">
 
             <div class="card">
+                
                 <div class="card-body">
+                <button class="btn btn-success btn-sm">Add Inventory</button>
                     <table class="table">
                         <thead>
                             <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">First</th>
-                                <th scope="col">Last</th>
-                                <th scope="col">Handle</th>
+                                <th scope="col">Category</th>
+                                <th scope="col">Item Name</th>
+                                <th scope="col">Qty</th>
+                                <th scope="col">Price</th>
+                                <th scope="col">Action</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
+                                <td>Computer</td>
+                                <td>Lenovo</td>
+                                <td>80</td>
+                                <td>45,846.00</td>
+                                <td>
+                                <button class="btn btn-info btn-sm">Edit</button>
+                                <button class="btn btn-danger btn-sm">Delete</button>
+                                </td>
                             </tr>
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">3</th>
-                                <td colspan="2">Larry the Bird</td>
-                                <td>@twitter</td>
-                            </tr>
+                        
                         </tbody>
                     </table>
                 </div>
